3068010bb1857218db4218743bcf9969a314580e
ronaldofas <ronaldodfas@gmail.com>
2022-12-26 16:02:10 -0300

    Implementação básica do simple injector.

--- exemplo-injecao-dependencia-csharp/Program.cs
+++ exemplo-injecao-dependencia-csharp/Program.cs
@@ -1,10 +1,26 @@
 ﻿using exemplo_injecao_dependencia_csharp.Infraestrutura_com_di;
-using exemplo_injecao_dependencia_csharp.Infraestrutura_sem_di;
+using exemplo_injecao_dependencia_csharp.Infraestrutura_com_di.Interfaces;
+using SimpleInjector;
 
 namespace exemplo_injecao_dependencia
 {
     public class exemplo_injecao_dependencia
     {
+        static readonly Container container;
+        static exemplo_injecao_dependencia()
+        {
+            // 1. Create a new Simple Injector container
+            container = new Container();
+
+            // 2. Configure the container (register)
+            container.Register<ILancadorDeDadosAleatoriosService_com_di, LancadorDeDadosAleatoriosServiceComDi>(Lifestyle.Singleton);
+            container.Register<IGeradorDeTelaService_com_di, GeradorDeTelaService_com_di>(Lifestyle.Singleton);
+            container.Register<app_com_di>();
+            
+            // 3. Verify your configuration
+            container.Verify();
+        }
+
         public static void Main(String[] args)
         {
             // Método sem injeção de dependencia
@@ -12,9 +28,13 @@ namespace exemplo_injecao_dependencia
             //app.Start();
 
             //Método com injeção de dependência
-            var lancador = new LancadorDeDadosAleatoriosServiceComDi();
-            var tela = new GeradorDeTelaService_com_di();
-            var app = new app_com_di(lancador, tela);
+            //var lancador = new LancadorDeDadosAleatoriosServiceComDi();
+            //var tela = new GeradorDeTelaService_com_di();
+            //var app = new app_com_di(lancador, tela);
+            //app.Start();
+
+            //Método com injeção de dependência e uso do simpleinjector
+            var app = container.GetInstance<app_com_di>();
             app.Start();
         }
     }
